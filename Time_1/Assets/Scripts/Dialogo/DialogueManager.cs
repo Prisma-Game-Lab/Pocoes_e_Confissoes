b6f5b077fefbc6a0938d20ef6b52534e84b2e6c7
tomytp <thomazrebellomiranda@gmail.com>
2022-03-09 22:45:34 -0300

    restore files

---
+++ Time_1/Assets/Scripts/Dialogo/DialogueManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+    public GameObject dialogueObject;
+	private Queue<string> sentences;
+
+	void Start () {
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue (Dialogue dialogue)
+	{
+        //SET ACTIVE
+        dialogueObject.SetActive(true);
+        
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence ()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(0.01f);
+		}
+	}
+
+	void EndDialogue()
+	{
+        dialogueObject.SetActive(false);
+	}
+
+}
