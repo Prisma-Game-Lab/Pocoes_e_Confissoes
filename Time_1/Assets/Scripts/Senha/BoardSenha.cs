b6a7dc4aea18968622aa54e051f8186d8eadc143
Thiago Cerqueira <thicerqs@gmail.com>
2022-03-10 18:02:40 -0300

    senha pt1

---
+++ Time_1/Assets/Scripts/Senha/BoardSenha.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSenha : MonoBehaviour
+{
+    [Header("Lista com Prefabs de Ingredientes")]
+    public GameObject[] ingredientes;
+
+    [Header("Layout do Board")]
+    public float primeiroX;
+    public float primeiroY;
+
+    private float atualX;
+    private float atualY;
+    private GameObject ingredienteAdd;
+
+    //Numero total de ingredientes já colocados nessa tentativa
+    private int totalInLine;
+    private int currentLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        atualX = primeiroX;
+        atualY = primeiroY;
+        totalInLine = 0;
+        currentLine = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Quando a linha encher, o proximo será colocado na debaixo
+        if (totalInLine >= 3)
+        {
+            totalInLine = 0;
+            atualX = primeiroX;
+            atualY -= 1.5f;
+            currentLine++;
+
+            if (currentLine == 6)
+            {
+                primeiroX = 6f;
+                atualX = primeiroX;
+                atualY = primeiroY;
+            }
+        }
+        
+    }
+
+    public void AddIngrediente(int ingNumber)
+    {
+        ingredienteAdd = Instantiate(ingredientes[ingNumber], new Vector3(atualX, atualY, 0f), Quaternion.identity);
+        totalInLine++;
+        atualX += 1.5f;
+    }
+}
