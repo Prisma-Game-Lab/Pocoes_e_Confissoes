873d4c34d3ca6b1efc55335b0e0bf257ea248ce5
tomytp <thomazrebellomiranda@gmail.com>
2022-03-10 19:08:26 -0300

    Dialogo com mais pessoas

--- Time_1/Assets/Scripts/Senha/ButtonTrigger.cs
+++ Time_1/Assets/Scripts/Senha/ButtonTrigger.cs
@@ -6,17 +6,6 @@ public class ButtonTrigger : MonoBehaviour
 {
     public int ingredientNumber;
     public BoardSenha boardScript;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnMouseDown()
     {
