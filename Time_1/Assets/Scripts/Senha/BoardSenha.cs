142a08a8ddc4d3dfd18537d83792368088b95039
tomytp <thomazrebellomiranda@gmail.com>
2022-03-24 21:47:26 -0300

    Audio Volume Controller

--- Time_1/Assets/Scripts/Senha/BoardSenha.cs
+++ Time_1/Assets/Scripts/Senha/BoardSenha.cs
@@ -130,4 +130,5 @@ public class BoardSenha : MonoBehaviour
         }
         RemoveIngredient();
     }
+    
 }
--- Time_1/Assets/Scripts/Settings/MenuController.cs
+++ Time_1/Assets/Scripts/Settings/MenuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,19 +9,24 @@ using TMPro;
 public class MenuController : MonoBehaviour
 {
     [Header("VolumeSettings")]
-    [SerializeField] private TextMeshProUGUI volumeTextValue;
-    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private TextMeshProUGUI MusicTextValue;
+    [SerializeField] private TextMeshProUGUI FXTextValue;
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider FXSlider;
+    [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private float defaultVolume;
+    [SerializeField] private float multiplier;
 
-    private void Awake()
+    private void Start()
     {
         LoadSettings();
     }
     public void LoadSettings()
     {
-        if (PlayerPrefs.HasKey("SoundFXVolume"))
+        if (PlayerPrefs.HasKey("SoundFXVolume") && PlayerPrefs.HasKey("MusicVolume"))
         {
-            SetVolume(PlayerPrefs.GetFloat("SoundFXVolume"));
+            SetFxVolume(PlayerPrefs.GetFloat("SoundFXVolume"));
+            SetMusicVolume(PlayerPrefs.GetFloat("MusicVolume"));
         }
         else
         {
@@ -28,29 +34,38 @@ public class MenuController : MonoBehaviour
         }
     }
 
-    public void SetVolume(float volume)
+    public void SetFxVolume(float volume)
     {
-        AudioListener.volume = volume/100f;
-        volumeSlider.value = volume;
-        volumeTextValue.text = volume.ToString();
+        audioMixer.SetFloat("SoundFXVolume", Mathf.Log10((volume != 0)? volume/100f : -80f) * multiplier);
         PlayerPrefs.SetFloat("SoundFXVolume", volume);
+        FXSlider.value = volume;
+        FXTextValue.text = volume.ToString();
     }
 
-    public void ResetValues(string s)
+    public void SetMusicVolume(float volume)
     {
-        switch (s)
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10((volume != 0)? volume/100f : -80f) * multiplier);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+        MusicSlider.value = volume;
+        MusicTextValue.text = volume.ToString();
+    }
+
+    public void ResetValues(string type)
+    {
+        switch (type)
         {
             case "audio":
-                SetVolume(defaultVolume);
+                SetFxVolume(defaultVolume);
+                SetMusicVolume(defaultVolume);
                 break;
-
             default:
-                SetVolume(defaultVolume);
+                SetFxVolume(defaultVolume);
+                SetMusicVolume(defaultVolume);
                 break;
         }
     }
 
-    public void ExitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
